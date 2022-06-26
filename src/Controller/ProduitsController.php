d01862a94f93ebe0413838783990510b552b0016
Makou46 <maxencelevillain18@gmail.com>
2022-06-26 21:34:17 +0200

    rajout de la page presentation

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProduitRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProduitsController extends AbstractController
 {
     /**
      * @Route("/produits", name="app_produits")
      */
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
+        $produits = $produitRepository->findAll();
+
         return $this->render('produits/produits.html.twig', [
             'controller_name' => 'ProduitsController',
+            'produits' => $produits,
+        ]);
+    }
+
+    /**
+     * @Route("/produits/{id}", name="app_produit")
+     */
+    public function singleProduit(): Response
+    {
+        return $this->render('produits/produits.html.twig', [
+            'controller_name' => 'SingleProduitController',
         ]);
     }
 }
